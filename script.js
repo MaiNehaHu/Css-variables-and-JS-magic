c7562fdfb416c2fefef49b6fbcc79c0eedf09fa3
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2023-02-07 05:12:40 +0530

    comments

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let inputs = document.querySelectorAll('.edits input')
 console.log(inputs);
 
 function HandleUpdate(){
-    let suffix = this.dataset.sizing
+    let suffix = this.dataset.sizing /**gives datatype i.e. pixels */
     document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
-    
+    /**important : this.name and css variable names must be same */
 
     console.log(this.name+" "+this.value +" "+suffix);
 }
